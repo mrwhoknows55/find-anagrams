f61f986d2ab69724f5f370ceb99048b285dea2be
mrwhoknows55 <avdhuttanugade55@gmail.com>
2023-01-02 23:12:29 +0530

    add: basic anagram finder logic

--- app/src/main/java/com/mrwhoknows/findanagrams/MainActivity.kt
+++ app/src/main/java/com/mrwhoknows/findanagrams/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.mrwhoknows.findanagrams
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.mrwhoknows.findanagrams.ui.theme.FindAnagramsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,10 +34,40 @@ class MainActivity : ComponentActivity() {
             FindAnagramsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        when (val state = viewModel.state.collectAsState().value) {
+                            is Resource.Failure -> {
+                                Log.d(TAG, "onCreate: Failure: ${state.msg}")
+                            }
+                            is Resource.Loading -> {
+                                Log.d(TAG, "onCreate: loading")
+                            }
+                            is Resource.Success -> {
+                                Log.d(TAG, "onCreate: success: ${state.data}")
+                                state.data?.let {
+                                    val goodWord = it.goodWord ?: ""
+                                    val guessedWords = it.guessedWords
+                                    Log.d(
+                                        TAG, "onCreate: anagramState: $it"
+                                    )
+                                    EditTx(goodWord) { word ->
+                                        viewModel.isGoodWord(goodWord, word)
+                                    }
+                                    LazyColumn {
+                                        items(guessedWords.size) { index ->
+                                            guessedWords[index].apply {
+                                                AnagramItem(name = first, isValid = second)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -35,14 +75,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AnagramItem(name: String, isValid: Boolean) {
+    Text(
+        text = "$name!", color = if (isValid) Color.Green else Color.Red
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    FindAnagramsTheme {
-        Greeting("Android")
-    }
+fun EditTx(
+    word: String, onSubmit: (String) -> Unit
+) {
+    var text by remember { mutableStateOf("") }
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        singleLine = true,
+        modifier = Modifier.padding(4.dp),
+        label = { Text(text = word) },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text, imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(onDone = {
+            onSubmit.invoke(text.trim())
+        })
+    )
 }
+
+private const val TAG = "MainActivity"
--- app/src/main/java/com/mrwhoknows/findanagrams/MainViewModel.kt
+++ app/src/main/java/com/mrwhoknows/findanagrams/MainViewModel.kt
@@ -1,23 +1,113 @@
 package com.mrwhoknows.findanagrams
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import java.io.BufferedReader
 import java.io.Reader
 import javax.inject.Inject
+import kotlin.random.Random
 
 @HiltViewModel
 class MainViewModel
 @Inject constructor(
     private val reader: Reader
 ) : ViewModel() {
+    private val wordToAnagrams = hashMapOf<String, MutableList<String>>()
     private val wordSet = hashSetOf<String>()
-    fun setup() {
-        val words = BufferedReader(reader).readLines()
-        wordSet.addAll(words)
-        Log.d(TAG, "words: $words")
+    private val wordList = mutableListOf<String>()
+
+    private var currentState: AnagramViewState = AnagramViewState()
+    private val _state: MutableStateFlow<Resource<AnagramViewState>> =
+        MutableStateFlow(Resource.Loading())
+    val state = _state.asStateFlow()
+
+    fun getAnagrams(sourceWord: String): Set<String> {
+        val result = hashSetOf<String>()
+        val sourceWordSorted = sourceWord.sortLetters()
+        for (word in wordSet) {
+            if (sourceWordSorted.equals(word.sortLetters(), true)) {
+                result.add(word)
+            }
+        }
+        wordToAnagrams[sourceWord] = result.toMutableList()
+        return result
     }
+
+    fun pickGoodStarterWord(minimumAnagram: Int = 4): String {
+        var toContinue = true
+        var word = ""
+        while (toContinue) {
+            val randomIndex = Random.nextInt(wordSet.size)
+            word = wordList[randomIndex]
+            val sortedWord = word.sortLetters()
+            val anagrams = wordToAnagrams[word] ?: mutableListOf()
+            val noOfAnagrams =
+                if (anagrams.isNotEmpty()) anagrams.size else getAnagrams(sortedWord).size
+            toContinue = noOfAnagrams <= minimumAnagram
+        }
+        return word
+    }
+    fun isGoodWord(baseWord: String, enteredWord: String) {
+        val isGoodWord =
+            !baseWord.equals(enteredWord, true) && getAnagrams(baseWord).contains(enteredWord)
+        val previousList = currentState.guessedWords.toMutableList()
+        previousList.add(Pair(enteredWord, isGoodWord))
+        currentState = currentState.copy(guessedWords = previousList)
+        viewModelScope.launch {
+            _state.emit(Resource.Success(currentState))
+        }
+    }
+
+    private var setupJob: Job? = null
+    fun setup() {
+        setupJob?.cancel()
+        setupJob = viewModelScope.launch(Dispatchers.IO) {
+            try {
+                _state.emit(Resource.Loading())
+                val input = BufferedReader(reader).readLines()
+                input.forEach {
+                    val word = it.trim()
+                    wordSet.add(word)
+                    val sortedWord = word.sortLetters()
+                    if (wordToAnagrams.containsKey(sortedWord) && !wordToAnagrams[sortedWord].isNullOrEmpty()) {
+                        wordToAnagrams[sortedWord]?.add(word)
+                    } else {
+                        wordToAnagrams[sortedWord] = mutableListOf(word)
+                    }
+                }
+                reader.close()
+                wordList.addAll(wordSet)
+                val goodWord = pickGoodStarterWord()
+                currentState = currentState.copy(
+                    goodWord = goodWord,
+                    guessedWords = emptyList(),
+                )
+                _state.emit(Resource.Success(currentState))
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _state.emit(Resource.Failure())
+            }
+        }
+    }
+
+    private fun String.sortLetters(): String = String(this.toCharArray().also { it.sort() })
+}
+
+data class AnagramViewState(
+    val goodWord: String? = null,
+    val guessedWords: List<Pair<String, Boolean>> = emptyList(),
+)
+
+sealed class Resource<T> {
+    data class Failure<T>(val msg: String = "Something Went Wrong!") : Resource<T>()
+    class Loading<T> : Resource<T>()
+    data class Success<T>(val data: T?) : Resource<T>()
 }
 
 private const val TAG = "MainViewModel"
